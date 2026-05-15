cbf13ec10704550ea25519b79e0a850d0dbae5e0
seoulhan <seoulhanknox@gmail.com>
2026-05-15 14:01:55 +0900

    initial files

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: C:/DEV/TDD_TV_15
+# Build directory: C:/DEV/TDD_TV_15/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("C:/DEV/TDD_TV_15/build/TunerTest[1]_include.cmake")
+include("C:/DEV/TDD_TV_15/build/TVControllerTest[1]_include.cmake")
+subdirs("_deps/googletest-build")
+subdirs("_deps/approvaltests-build")
